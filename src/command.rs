e0b271cca84905f7c7e5cddbfc8c6686f854ae92
Christian Jaeger <ch@christianjaeger.ch>
2024-12-20 21:52:32 +0100

    command: allow to specify capturing, return Outputs with impl Display
    - Add struct Capturing to specify capturing mode.
    - Add struct Outputs to capture the output and status together with
      the truthy boolean, and `Display` support.
    - Add `run_output` to return `Output`.
    - Add `run_outputs` to return `Outputs` and implicitly capturing both.
    - `run_stdout`, `run_stderr` now return `Outputs`
    
    Internally:
    
    - add struct DisplayOutput
    - add lossy_string, display_output, check_exitstatus_context

--- src/command.rs
+++ src/command.rs
@@ -2,13 +2,20 @@
 //! external programs.
 
 use std::{
+    borrow::Cow,
     ffi::OsStr,
-    fmt::Debug,
+    fmt::{Debug, Display},
+    ops::Deref,
     path::Path,
-    process::{Child, Command, ExitStatus},
+    process::{Child, Command, ExitStatus, Output, Stdio},
 };
 
 use anyhow::{anyhow, bail, Context, Result};
+
+fn lossy_string(v: &[u8]) -> Cow<str> {
+    // XX different on Windows?
+    String::from_utf8_lossy(v)
+}
 
 fn cmd_args<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
     cmd: P,
@@ -23,52 +30,213 @@ fn cmd_args<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
     vec
 }
 
-fn check_exitstatus(
-    exitstatus: &ExitStatus,
-    acceptable_status_codes: &[i32],
-    get_cmd_args_dir: &dyn Fn() -> (Vec<String>, String),
-) -> Result<bool> {
+fn check_exitstatus(exitstatus: &ExitStatus, acceptable_status_codes: &[i32]) -> Result<bool> {
     if let Some(code) = exitstatus.code() {
         if acceptable_status_codes.contains(&code) {
             Ok(code == 0)
         } else {
-            let (cmd_args, in_dir) = get_cmd_args_dir();
-            bail!(
-                "running {cmd_args:?} in directory {in_dir:?}: \
-                 command exited with code {code}",
-            )
+            bail!("command exited with code {code}",)
         }
     } else {
-        let (cmd_args, in_dir) = get_cmd_args_dir();
-        bail!(
-            "running {cmd_args:?} in directory {in_dir:?}: command exited via signal, \
-             or other problem",
-        )
+        bail!("command exited via signal, or other problem",)
     }
+}
+
+/// Which outputs and how they should be captured.
+#[derive(Debug)]
+pub struct Capturing {
+    stdout: Option<Stdio>,
+    stderr: Option<Stdio>,
+}
+
+impl Capturing {
+    pub fn none() -> Self {
+        Self {
+            stdout: None,
+            stderr: None,
+        }
+    }
+    pub fn stdout() -> Self {
+        Self {
+            stdout: Some(Stdio::piped()),
+            stderr: None,
+        }
+    }
+    pub fn stderr() -> Self {
+        Self {
+            stdout: None,
+            stderr: Some(Stdio::piped()),
+        }
+    }
+    pub fn both() -> Self {
+        Self {
+            stdout: Some(Stdio::piped()),
+            stderr: Some(Stdio::piped()),
+        }
+    }
+    pub fn available(&self) -> AvailableCaptures {
+        match self {
+            Self {
+                stdout: None,
+                stderr: None,
+            } => AvailableCaptures::None,
+            Self {
+                stdout: Some(_),
+                stderr: None,
+            } => AvailableCaptures::Stdout,
+            Self {
+                stdout: None,
+                stderr: Some(_),
+            } => AvailableCaptures::Stderr,
+            Self {
+                stdout: Some(_),
+                stderr: Some(_),
+            } => AvailableCaptures::Both,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum AvailableCaptures {
+    Stdout,
+    Stderr,
+    Both,
+    None,
+}
+
+/// Wrapper around `Output` that dereferences to it, but also offers a
+/// `truthy` field, and implements `Display` to show a multi-line message
+/// with both process status and its outputs.
+#[derive(Debug)]
+pub struct Outputs<'t> {
+    /// Whether process exited with status 0 (or maybe in the future a
+    /// set of status codes that represent success)
+    pub truthy: bool,
+    pub available_captures: AvailableCaptures,
+    pub output: Output,
+    /// Prefixed to `output` lines, "\t" by default
+    pub indent: &'t str,
+}
+
+impl<'t> Deref for Outputs<'t> {
+    type Target = Output;
+
+    fn deref(&self) -> &Self::Target {
+        &self.output
+    }
+}
+
+fn display_output(
+    output: &Output,
+    available_captures: AvailableCaptures,
+    f: &mut std::fmt::Formatter<'_>,
+    indent: &str,
+) -> std::fmt::Result {
+    match available_captures {
+        AvailableCaptures::Stdout => f.write_fmt(format_args!(
+            "{},\n{indent}stdout: {}",
+            output.status,
+            lossy_string(&output.stdout),
+        )),
+        AvailableCaptures::Stderr => f.write_fmt(format_args!(
+            "{},\n{indent}stderr: {}",
+            output.status,
+            lossy_string(&output.stderr)
+        )),
+        AvailableCaptures::Both => f.write_fmt(format_args!(
+            "{},\n{indent}stdout: {}\n{indent}stderr: {}",
+            output.status,
+            lossy_string(&output.stdout),
+            lossy_string(&output.stderr)
+        )),
+        AvailableCaptures::None => f.write_fmt(format_args!("{}", output.status,)),
+    }
+}
+
+impl<'t> Display for Outputs<'t> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        display_output(&self.output, self.available_captures, f, self.indent)
+    }
+}
+
+// And a temporary helper to re-use `display_output` when truthy is
+// not available
+struct DisplayOutput<'t> {
+    available_captures: AvailableCaptures,
+    output: &'t Output,
+    indent: &'t str,
+}
+
+impl<'t> Display for DisplayOutput<'t> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        display_output(&self.output, self.available_captures, f, self.indent)
+    }
+}
+
+fn check_exitstatus_context<'t, P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
+    in_directory: &'t Path,
+    cmd: &'t P,
+    arguments: &'t [A],
+    available_captures: AvailableCaptures,
+    output: &'t Output,
+) -> impl Fn() -> anyhow::Error + 't {
+    move || {
+        let (cmd_args, in_dir, output) = (
+            cmd_args(&cmd, arguments),
+            in_directory.to_string_lossy().to_string(),
+            DisplayOutput {
+                available_captures,
+                output: &output,
+                indent: "\t",
+            },
+        );
+        anyhow!("running {cmd_args:?} in directory {in_dir:?}, {output}")
+    }
+}
+
+pub fn command_with_settings<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
+    in_directory: &Path,
+    cmd: P,
+    arguments: &[A],
+    set_env: &[(&str, &str)],
+    captures: Capturing,
+) -> Command {
+    let mut c = Command::new(&cmd);
+    c.args(arguments).current_dir(in_directory);
+    for (k, v) in set_env {
+        c.env(k, v);
+    }
+    if let Some(cap) = captures.stdout {
+        c.stdout(cap);
+    }
+    if let Some(cap) = captures.stderr {
+        c.stderr(cap);
+    }
+    c
 }
 
 /// Run `cmd` with `arguments` and the env overridden with the
 /// key-value pairs in `set_env`, return a `Child` handle (does *not*
-/// wait for its completion).
+/// wait for its completion). If you don't want to capture outputs,
+/// pass `Captures::none()` to `captures`. Note: if you just want to
+/// run a process in the background and are not actually reading from
+/// the filehandles in `Child`, then you definitely want to set this
+/// to `Captures::none()`, because otherwise the outputs will go
+/// nowhere, or even block the child process after filling the pipe
+/// buffer.
 pub fn spawn<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
     in_directory: &Path,
     cmd: P,
     arguments: &[A],
     set_env: &[(&str, &str)],
+    captures: Capturing,
 ) -> Result<Child> {
-    let get_cmd_args_dir = || {
-        (
+    let mut c = command_with_settings(in_directory, &cmd, arguments, set_env, captures);
+    c.spawn().with_context(|| {
+        let (cmd_args, in_dir) = (
             cmd_args(&cmd, arguments),
             in_directory.to_string_lossy().to_string(),
-        )
-    };
-    let mut c1 = Command::new(&cmd);
-    c1.args(arguments).current_dir(in_directory);
-    for (k, v) in set_env {
-        c1.env(k, v);
-    }
-    c1.spawn().with_context(|| {
-        let (cmd_args, in_dir) = get_cmd_args_dir();
+        );
         anyhow!("running {cmd_args:?} in directory {in_dir:?}",)
     })
 }
@@ -92,12 +260,64 @@ pub fn run<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
             in_directory.to_string_lossy().to_string(),
         )
     };
-    let mut c = spawn(in_directory, &cmd, arguments, set_env)?;
+    let mut c = spawn(in_directory, &cmd, arguments, set_env, Capturing::none())?;
     let exitstatus = c.wait().with_context(|| {
         let (cmd_args, in_dir) = get_cmd_args_dir();
         anyhow!("running {cmd_args:?} in directory {in_dir:?}",)
     })?;
-    check_exitstatus(&exitstatus, acceptable_status_codes, &get_cmd_args_dir)
+    check_exitstatus(&exitstatus, acceptable_status_codes).with_context(|| {
+        let (cmd_args, in_dir) = get_cmd_args_dir();
+        anyhow!("running {cmd_args:?} in directory {in_dir:?}",)
+    })
+}
+
+pub fn run_output<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
+    in_directory: &Path,
+    cmd: P,
+    arguments: &[A],
+    set_env: &[(&str, &str)],
+    captures: Capturing,
+) -> Result<Output> {
+    let mut c = command_with_settings(in_directory, &cmd, arguments, set_env, captures);
+    c.output().with_context(|| {
+        let (cmd_args, in_dir) = (
+            cmd_args(&cmd, arguments),
+            in_directory.to_string_lossy().to_string(),
+        );
+        anyhow!("running {cmd_args:?} in directory {in_dir:?}",)
+    })
+}
+
+/// Same as `run` but captures outputs, returning (exited_0, stdout,
+/// stderr)
+pub fn run_outputs<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
+    in_directory: &Path,
+    cmd: P,
+    arguments: &[A],
+    set_env: &[(&str, &str)],
+    acceptable_status_codes: &[i32],
+) -> Result<Outputs<'static>> {
+    let captures = Capturing::both();
+    let available_captures = captures.available();
+    let output = run_output(in_directory, &cmd, arguments, set_env, captures)?;
+    let truthy = check_exitstatus(&output.status, acceptable_status_codes).with_context(|| {
+        let (cmd_args, in_dir, output) = (
+            cmd_args(&cmd, arguments),
+            in_directory.to_string_lossy().to_string(),
+            DisplayOutput {
+                available_captures,
+                output: &output,
+                indent: "\t",
+            },
+        );
+        anyhow!("running {cmd_args:?} in directory {in_dir:?}, {output}")
+    })?;
+    Ok(Outputs {
+        output,
+        truthy,
+        available_captures,
+        indent: "\t\t",
+    })
 }
 
 /// Same as `run` but captures and returns stdout.
@@ -107,24 +327,48 @@ pub fn run_stdout<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
     arguments: &[A],
     set_env: &[(&str, &str)],
     acceptable_status_codes: &[i32],
-) -> Result<Vec<u8>> {
-    let get_cmd_args_dir = || {
-        (
+) -> Result<Outputs<'static>> {
+    let captures = Capturing::stdout();
+    let available_captures = captures.available();
+    let output = run_output(in_directory, &cmd, arguments, set_env, captures)?;
+    let truthy = check_exitstatus(&output.status, acceptable_status_codes).with_context(|| {
+        let (cmd_args, in_dir, output) = (
             cmd_args(&cmd, arguments),
             in_directory.to_string_lossy().to_string(),
-        )
-    };
-    // Can't use spawn here, right? XX refactor into yet another
-    // sub-function before calling spawn?
-    let mut c1 = Command::new(&cmd);
-    c1.args(arguments).current_dir(in_directory);
-    for (k, v) in set_env {
-        c1.env(k, v);
-    }
-    let c = c1.output().with_context(|| {
-        let (cmd_args, in_dir) = get_cmd_args_dir();
-        anyhow!("running {cmd_args:?} in directory {in_dir:?}",)
+            DisplayOutput {
+                available_captures,
+                output: &output,
+                indent: "\n",
+            },
+        );
+        anyhow!("running {cmd_args:?} in directory {in_dir:?}, {output}")
     })?;
-    check_exitstatus(&c.status, acceptable_status_codes, &get_cmd_args_dir)?;
-    Ok(c.stdout)
+    Ok(Outputs {
+        output,
+        truthy,
+        available_captures,
+        indent: "\t\t",
+    })
+}
+
+/// Same as `run` but captures and returns stderr.
+pub fn run_stderr<P: AsRef<OsStr> + Debug, A: AsRef<OsStr> + Debug>(
+    in_directory: &Path,
+    cmd: P,
+    arguments: &[A],
+    set_env: &[(&str, &str)],
+    acceptable_status_codes: &[i32],
+) -> Result<Outputs<'static>> {
+    let captures = Capturing::stderr();
+    let available_captures = captures.available();
+    let output = run_output(in_directory, &cmd, arguments, set_env, captures)?;
+    let truthy = check_exitstatus(&output.status, acceptable_status_codes).with_context(
+        check_exitstatus_context(in_directory, &cmd, arguments, available_captures, &output),
+    )?;
+    Ok(Outputs {
+        output,
+        truthy,
+        available_captures,
+        indent: "\t\t",
+    })
 }
