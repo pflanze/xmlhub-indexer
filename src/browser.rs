dc45e06b7dfc2f4d7b7f9e68ced64087130a17fb
Christian Jaeger <ch@christianjaeger.ch>
2024-12-20 13:47:03 +0100

    browsers: implement for macOS

--- src/browser.rs
+++ src/browser.rs
@@ -1,44 +1,159 @@
-use std::{env, ffi::OsStr, path::Path, process::Child};
+use std::{
+    env,
+    ffi::{OsStr, OsString},
+    ops::Deref,
+    path::Path,
+    process::Child,
+};
 
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 
-use crate::{command::spawn, util::flatten};
+use crate::{
+    command::{run, spawn},
+    util::append,
+};
 
-/// Find a web browser and run it with the given arguments. Tries
-/// `sensible-browser`, the browsers specified in the `BROWSER`
-/// environment variable, which is split on ':' into program names or
-/// paths that are tried in order, `firefox`, `chromium` or
-/// `chrome`. Fails if none worked.
-pub fn spawn_browser(in_directory: &Path, arguments: &[&OsStr]) -> Result<Child> {
-    let prolonged_ownership_val;
-    let env_browsers: Vec<&str> = match env::var("BROWSER") {
+fn to_owned_items<O, T: ToOwned<Owned = O> + ?Sized>(vals: &[&T]) -> Vec<O> {
+    vals.iter().map(|s| (*s).to_owned()).collect::<Vec<O>>()
+}
+
+const LINUX_BROWSERS: &[&str] = &["sensible-browser", "firefox", "chromium", "chrome"];
+
+enum BrowsersSource {
+    Env,
+    HardCoded,
+}
+
+impl BrowsersSource {
+    fn to_str(self) -> &'static str {
+        match self {
+            BrowsersSource::Env => "BROWSER env variable",
+            BrowsersSource::HardCoded => "hard coded browsers list",
+        }
+    }
+}
+
+fn get_browsers() -> Result<(BrowsersSource, Vec<String>)> {
+    let linux_browsers = || {
+        (
+            BrowsersSource::HardCoded,
+            LINUX_BROWSERS
+                .iter()
+                .map(Deref::deref)
+                .map(ToOwned::to_owned)
+                .collect(),
+        )
+    };
+    match env::var("BROWSER") {
         Ok(val) => {
-            prolonged_ownership_val = val;
-            prolonged_ownership_val.split(':').collect()
+            let bs: Vec<_> = val
+                .split(':')
+                .filter(|s| !s.is_empty())
+                .map(ToOwned::to_owned)
+                .collect();
+            Ok(if bs.is_empty() {
+                linux_browsers()
+            } else {
+                (BrowsersSource::Env, bs)
+            })
         }
         Err(e) => match e {
-            env::VarError::NotPresent => vec![],
+            env::VarError::NotPresent => Ok(linux_browsers()),
             env::VarError::NotUnicode(_) => bail!("reading BROWSER env var: {e}"),
         },
-    };
-    let choices = flatten([
-        // Due to using spawn, we stop the search if
-        // "sensible-browser" (a Debian thing) is found; thus we
-        // try that first, then if not available, try the BROWSER
-        // items. "sensible-browser" also tries the BROWSER
-        // entries, so it would be duplication here, but we know
-        // at that point "sensible-browser" hasn't tried because
-        // it wasn't executed.
-        ["sensible-browser"].as_ref(),
-        env_browsers.as_ref(),
-        ["firefox", "chromium", "chrome"].as_ref(),
-    ]);
+    }
+}
+
+pub fn spawn_browser_linux(in_directory: &Path, arguments: &[&OsStr]) -> Result<Child> {
+    let (browsers_source, browsers) = get_browsers()?;
+
     let mut errors = Vec::new();
-    for browser in &choices {
-        match spawn(in_directory, *browser, arguments, &[]) {
+    for browser in &browsers {
+        match spawn(in_directory, browser, arguments, &[]) {
             Ok(handle) => return Ok(handle),
             Err(e) => errors.push(format!("{e:#}")),
         }
     }
-    bail!("could not find a web browser, tried: {choices:?} (errors: {errors:?})")
+    bail!(
+        "could not find a web browser, tried {browsers:?} from {}:\n{}",
+        browsers_source.to_str(),
+        errors
+            .iter()
+            .map(|e| format!("\t{e}"))
+            .collect::<Vec<_>>()
+            .join("\n")
+    )
+}
+
+/// Find a web browser and run it with the given arguments, working on
+/// MacOS. Tries a few other browsers than Safari first.
+pub fn spawn_browser_macos(in_directory: &Path, arguments: &[&OsStr]) -> Result<()> {
+    let (browsers_source, mut browsers) = get_browsers()?;
+    match browsers_source {
+        BrowsersSource::Env => (),
+        BrowsersSource::HardCoded => {
+            // First try the default action? But that will presumably
+            // always succeed, but "worse", that may be a text editor
+            // or so. Thus instead try the normal alternative browser
+            // names first, then also Safari:
+            browsers.push("safari".into());
+        }
+    }
+
+    let mut errors = Vec::new();
+    for browser in &browsers {
+        let all_arguments = append(
+            &[OsString::from("-a"), OsString::from(browser)],
+            &to_owned_items(arguments),
+        );
+
+        let may_be_gui_program_name = match &browsers_source {
+            BrowsersSource::Env => !browser.contains('/'),
+            BrowsersSource::HardCoded => true,
+        };
+
+        if may_be_gui_program_name {
+            if run(in_directory, "open", &all_arguments, &[], &[0, 1]).with_context(|| {
+                anyhow!("starting a browser, trying 'open' with argument {all_arguments:?}")
+            })? {
+                return Ok(());
+            }
+            errors.push(format!("not found when executed via open -a"));
+        }
+
+        // Try as path or program name via $PATH instead
+        match spawn(in_directory, browser, arguments, &[]) {
+            Ok(_handle) => return Ok(()),
+            Err(e) => errors.push(format!("error when executed directly: {e:#}")),
+        }
+    }
+    bail!(
+        "could not find a web browser, tried {browsers:?} from {}:\n{}",
+        browsers_source.to_str(),
+        errors
+            .iter()
+            .map(|e| format!("\t{e}"))
+            .collect::<Vec<_>>()
+            .join("\n")
+    )
+}
+
+/// Find a web browser and run it with the given arguments. If the
+/// `BROWSER` environment variable is set, splits it on ':' into
+/// browser names or paths and tries executing those (if names, on
+/// MacOS via `open -a`). Otherwise tries "sensible-browser",
+/// "firefox", "chromium", "chrome" in turn. Fails if none could be
+/// started or an env variable could not be decoded as UTF-8.
+pub fn spawn_browser(in_directory: &Path, arguments: &[&OsStr]) -> Result<()> {
+    match std::env::consts::OS {
+        "macos" => spawn_browser_macos(in_directory, arguments),
+        _ => match std::env::consts::FAMILY {
+            "unix" => {
+                spawn_browser_linux(in_directory, arguments)?;
+                Ok(())
+            }
+            // "windows" =>
+            s => bail!("spawn_browser: don't know how to handle OS family {s:?}"),
+        },
+    }
 }
