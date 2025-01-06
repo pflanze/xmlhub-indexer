50163dd4aecc85c134368e3f367602930a70e9eb
Christian Jaeger <ch@christianjaeger.ch>
2025-01-06 12:51:13 +0100

    parse_xml: don't read into memory first

--- src/parse_xml.rs
+++ src/parse_xml.rs
@@ -1,4 +1,8 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{BufReader, Seek},
+    path::Path,
+};
 
 use anyhow::{anyhow, Context, Result};
 use xml::{reader::XmlEvent, EventReader, ParserConfig};
@@ -10,11 +14,12 @@ use xmltree::Element;
 /// of the Element tree, it could be a good idea to generate it to
 /// detect when a file is not well-formed XML.
 pub fn parse_xml_file(path: &Path, build_tree: bool) -> Result<(Vec<String>, Option<Element>)> {
-    let bytes = std::fs::read(path).with_context(|| anyhow!("reading file {path:?}"))?;
+    let mut inp =
+        BufReader::new(File::open(path).with_context(|| anyhow!("reading file {path:?}"))?);
 
     // Parse `bytes` as item stream to extract the comments.
     let config = ParserConfig::new().ignore_comments(false);
-    let input = EventReader::new_with_config(&*bytes, config);
+    let input = EventReader::new_with_config(&mut inp, config);
     let mut comments = Vec::new();
     for item in input {
         let item = item.with_context(|| anyhow!("parsing file {path:?}"))?;
@@ -32,7 +37,8 @@ pub fn parse_xml_file(path: &Path, build_tree: bool) -> Result<(Vec<String>, Opt
 
     // Parse the bytes again, now building an element tree.
     let xmldoc = if build_tree {
-        Some(Element::parse(&*bytes).with_context(|| anyhow!("reparsing file {path:?}"))?)
+        inp.rewind()?;
+        Some(Element::parse(&mut inp).with_context(|| anyhow!("reparsing file {path:?}"))?)
     } else {
         None
     };
