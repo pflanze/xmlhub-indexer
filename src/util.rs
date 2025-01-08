bec7cc94c91249f45f65a17e6749f6234ebce231
Christian Jaeger <ch@christianjaeger.ch>
2025-01-08 21:35:36 +0100

    Docs

--- src/util.rs
+++ src/util.rs
@@ -175,8 +175,9 @@ pub fn create_dir_levels_if_necessary(dir_path: &Path, levels: u32) -> Result<()
     }
 }
 
-// Like `haystack.contains(needle)` for strings. (This will be in std
-// in the future: https://github.com/rust-lang/rust/issues/134149)
+// Does the same for bytes that `haystack.contains(needle)` does for
+// strings. (This will be in std in the future:
+// https://github.com/rust-lang/rust/issues/134149)
 pub fn contains_bytes(haystack: &[u8], needle: &[u8]) -> bool {
     haystack
         .windows(needle.len())
