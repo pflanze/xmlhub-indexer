c606b117c8d8e4d108b4c01be6af1b45b303feb0
Christian Jaeger <ch@christianjaeger.ch>
2025-01-17 01:11:13 +0100

    read_xml: use mmap instead of read
    Turns out it is slower in this context, too.

--- src/read_xml.rs
+++ src/read_xml.rs
@@ -1,21 +1,29 @@
-use std::path::Path;
+use std::{fs::File, path::Path};
 
 use anyhow::{Context, Result};
+use memmap2::Mmap;
+use ouroboros::self_referencing;
 use roxmltree::{Document, ParsingOptions};
 
 /// Representation of file contents that can be parsed from.
+#[self_referencing]
 pub struct XMLDocumentBacking {
-    string: String,
+    file: File,
+    #[borrows(file)]
+    map: Mmap,
+    #[borrows(map)]
+    string: &'this str,
 }
 
 impl XMLDocumentBacking {
     /// Read the file from the given path.
     pub fn from_path(path: &Path) -> Result<Self> {
-        // Back to reading the whole file to memory first since roxmltree
-        // requires that.
-        Ok(Self {
-            string: std::fs::read_to_string(path).context("reading file")?,
-        })
+        let file = File::open(path).context("opening file for reading")?;
+        Ok(Self::try_new::<anyhow::Error>(
+            file,
+            |file: &File| Ok(unsafe { Mmap::map(file) }?),
+            |map: &Mmap| Ok(std::str::from_utf8(&map[..])?),
+        )?)
     }
 
     /// Parse the given file, return the comments *above the top
@@ -26,8 +34,8 @@ impl XMLDocumentBacking {
             allow_dtd: true,
             ..ParsingOptions::default()
         };
-        let xmldoc =
-            Document::parse_with_options(&self.string, opt).context("parsing the XML markup")?;
+        let xmldoc = Document::parse_with_options(&*self.borrow_string(), opt)
+            .context("parsing the XML markup")?;
 
         let root = xmldoc.root();
 
