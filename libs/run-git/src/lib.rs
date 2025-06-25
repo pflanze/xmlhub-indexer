09dca513689287d8cc7f17eed0d1fc5ddb8ff647
Christian Jaeger <ch@christianjaeger.ch>
2025-06-25 10:41:21 +0200

    util: add empty!

--- libs/run-git/src/lib.rs
+++ libs/run-git/src/lib.rs
@@ -4,3 +4,13 @@ pub mod flattened;
 pub mod git;
 pub mod path_util;
 pub mod util;
+
+/// In situation requiring an array or slice with a generic type for
+/// the items, passing the empty array or slice is a bit cumbersome,
+/// e.g. `&[] as &[&str]`. This macro may or may not make this nicer.
+#[macro_export]
+macro_rules! empty {
+    [$T:ty] => {
+        {&[] as &[$T]}
+    }
+}
