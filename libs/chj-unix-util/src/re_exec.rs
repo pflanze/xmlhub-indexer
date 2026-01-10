c64ae7162133dc6b8bdc54daf609a7f179cf0b67
Christian Jaeger <ch@christianjaeger.ch>
2026-01-10 22:01:03 +0100

    re_exec.rs: add `re_exec`

--- libs/chj-unix-util/src/re_exec.rs
+++ libs/chj-unix-util/src/re_exec.rs
@@ -2,7 +2,9 @@ use std::os::unix::process::CommandExt;
 use std::path::PathBuf;
 use std::process::Command;
 
-pub fn re_exec_with_existing_args_and_env(executable_path: PathBuf) -> std::io::Error {
+use anyhow::{Context, Result};
+
+pub fn re_exec_with_executable(executable_path: PathBuf) -> std::io::Error {
     let mut args = std::env::args_os();
     let arg0 = args.next();
     let mut cmd = Command::new(&executable_path);
@@ -11,4 +13,18 @@ pub fn re_exec_with_existing_args_and_env(executable_path: PathBuf) -> std::io::
         cmd.arg0(a0);
     }
     cmd.exec()
+}
+
+/// Only returns when there is an error.
+pub fn _re_exec() -> Result<()> {
+    let path = std::env::current_exe().context("getting the path to the current executable")?;
+    Err(re_exec_with_executable(path.clone())).context("executing the binary {path:?}")
+}
+
+/// Only returns when there is an error.
+pub fn re_exec() -> anyhow::Error {
+    match _re_exec() {
+        Ok(()) => unreachable!(),
+        Err(e) => e,
+    }
 }
