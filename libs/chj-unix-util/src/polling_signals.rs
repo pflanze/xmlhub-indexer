e1c92863b632bd9f06ef8747739ab4b8b740b057
Christian Jaeger <ch@christianjaeger.ch>
2026-02-11 19:13:02 +0100

    chj-unix-util polling_signals.rs: add `SharedPollingSignals`

--- libs/chj-unix-util/src/polling_signals.rs
+++ libs/chj-unix-util/src/polling_signals.rs
@@ -85,6 +85,16 @@ impl IPCAtomicU64 {
     pub fn inc(&self) -> u64 {
         self.atomic().fetch_add(1, Ordering::SeqCst)
     }
+
+    /// Returns the last-seen value from reading when `f` returns None
+    /// as Err; returns Ok with the new value if `f` returned Some
+    /// (and storing succeeded--if it failed, it retries until `f`
+    /// returns None).
+    #[inline]
+    pub fn fetch_update(&self, f: impl FnMut(u64) -> Option<u64>) -> Result<u64, u64> {
+        self.atomic()
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, f)
+    }
 }
 
 /// A filesystem path based way to poll for 'signals'. Cloning makes
@@ -96,8 +106,23 @@ pub struct PollingSignals {
     atomic: Arc<IPCAtomicU64>,
 }
 
-/// Sending-only 'end' of a `PollingSignals`. Can be cloned to give
-/// yet another sending end.
+/// A filesystem path based way to poll for 'signals'. When receiving
+/// a signal, it has to be confirmed explicitly, and the confirmation
+/// is shared amongst all readers (usable for "work was done" on a
+/// 'versioned' input; the work confirmed refers to the version of the
+/// signal that was read; the confirmation with the highest signal
+/// value is stored). Note that multiple readers might start doing the
+/// same work at the same time. Cloning shares the same receiver and
+/// sender.
+#[derive(Debug, Clone)]
+pub struct SharedPollingSignals {
+    done: Arc<IPCAtomicU64>,
+    atomic: Arc<IPCAtomicU64>,
+}
+
+/// Sending-only 'end' of a `PollingSignals` or
+/// `SharedPollingSignals`. Can be cloned to give yet another sending
+/// end.
 #[derive(Debug, Clone)]
 pub struct PollingSignalsSender {
     atomic: Arc<IPCAtomicU64>,
@@ -142,6 +167,81 @@ impl PollingSignals {
         let atomic = self.atomic.clone();
         PollingSignalsSender { atomic }
     }
+}
+
+/// A received signal. This is a (dynamically-checked) 'linear type'
+/// value: it must be confirmed via `confirm` or `ignore`; dropping it
+/// panics!
+#[derive(Debug)]
+#[must_use]
+pub struct Signal<'t> {
+    seen: u64,
+    done: &'t IPCAtomicU64,
+    // careful: this struct is forgotten, do not add any non-Copy
+    // types!
+}
+
+impl<'t> Drop for Signal<'t> {
+    fn drop(&mut self) {
+        panic!("{self:?} must be passed to the confirm or ignore methods but was dropped")
+    }
+}
+
+impl<'t> Signal<'t> {
+    /// Returns true if we prevailed, false if another actor updated
+    /// past us (which is usually fine, too).
+    pub fn confirm(self) -> bool {
+        let Self { seen, done } = self;
+        std::mem::forget(self);
+        match done.fetch_update(|new_seen| if seen > new_seen { Some(seen) } else { None }) {
+            Ok(_) => true,
+            Err(_) => false,
+        }
+    }
+
+    pub fn ignore(self) {
+        std::mem::forget(self)
+    }
+}
+
+impl SharedPollingSignals {
+    pub fn open(
+        channel_path: &Path,
+        done_path: &Path,
+        initial_value: u64,
+    ) -> Result<Self, IPCAtomicError> {
+        let atomic = IPCAtomicU64::open(channel_path, initial_value)?.into();
+        let done = IPCAtomicU64::open(done_path, initial_value)?.into();
+        Ok(Self { done, atomic })
+    }
+
+    /// If there were signals, returns a representation of the last
+    /// one. `confirm` or `ignore` must be called on it when the
+    /// action warranted by the signal has been carried out, dropping
+    /// it will panic!
+    pub fn get_latest_signal(&self) -> Option<Signal<'_>> {
+        let Self { done, atomic } = self;
+        // XX these are two separate loads. Still OK since loading
+        // with SeqCst and we have a sequence?
+        let seen = atomic.load();
+        let done_value = done.load();
+        if seen > done_value {
+            Some(Signal {
+                seen,
+                done: &*self.done,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn sender(&self) -> PollingSignalsSender {
+        let atomic = self.atomic.clone();
+        PollingSignalsSender { atomic }
+    }
+
+    // Do we really want a send_signal() method here? Just always use
+    // sender().send_signal() instead?
 }
 
 impl PollingSignalsSender {
